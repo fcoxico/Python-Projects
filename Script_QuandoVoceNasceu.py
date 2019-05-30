577cc59fe52f272b05ad06e5678e9d7b485e7736
Francisco Jose Cardoso <franciscojosecardoso@gmail.com>
2019-05-30 11:03:15 -0300

    First Commit

---
+++ Script_QuandoVoceNasceu.py
@@ -0,0 +1,6 @@
+# Script quando você nasceu?
+
+dia = input ("Qual é o dia do seu nascimento ? ")
+mes = input ("Qual é o mês do seu nascimento ? ")
+ano = input ("Qual é o ano do seu nascimento ? ")
+print ("Você nasceu em ", dia , "/", mes,"/", ano, ". Correto?")
---
+++ Script_SomaDoisNumeros.py
@@ -0,0 +1,6 @@
+# Soma entre dois números
+
+numero1 = int(input ("Digite o primeiro número "))
+numero2 = int(input ("Digite o primeiro número "))
+soma = numero1 + numero2
+print ("A soma do numero", numero1, "+", numero2, "é", soma)
